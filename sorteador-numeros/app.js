2f665cf064e8e115b6694fe71e7602598ca78364
Hanna <79318688+Honey-lee429@users.noreply.github.com>
2025-07-30 22:00:18 -0300

    Update app.js

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -15,46 +15,46 @@ quantidadeInput.addEventListener('input', () => {
   */
     
 
-  function sortear() {
-        let qt = parseInt(quantidadeInput.value);
-        let de = parseInt(deInput.value);
-        let ate = parseInt(ateInput.value);
-      
-        if (isNaN(qt) || isNaN(de) || isNaN(ate) || qt <= 0 || de >= ate) {
-            numerosSorteados.textContent = 'Valores inválidos. Verifique os campos.';
-            return;
-        }
-
-        // Set garante que os números sorteados sejam únicos
-        let numeros = new Set();
-        while (numeros.size < qt) {
-            let numeroAleatorio = Math.floor(Math.random() * (ate - de + 1)) + de;
-            numeros.add(numeroAleatorio);
-        }
-
-        const resultadoFinal = Array.from(numeros).sort((a, b) => a - b);
-       numerosSorteados.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${resultadoFinal.join(', ')} </label>`;
-       alterarStatusBotaoReiniciar();
-
+function sortear() {
+    let qt = parseInt(quantidadeInput.value);
+    let de = parseInt(deInput.value);
+    let ate = parseInt(ateInput.value);
+  
+    if (isNaN(qt) || isNaN(de) || isNaN(ate) || qt <= 0 || de >= ate) {
+        numerosSorteados.textContent = 'Valores inválidos. Verifique os campos.';
+        return;
     }
 
+    // Set garante que os números sorteados sejam únicos
+    let numeros = new Set();
+    while (numeros.size < qt) {
+        let numeroAleatorio = Math.floor(Math.random() * (ate - de + 1)) + de;
+        numeros.add(numeroAleatorio);
+    }
+
+    const resultadoFinal = Array.from(numeros).sort((a, b) => a - b);
+   numerosSorteados.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${resultadoFinal.join(', ')} </label>`;
+   alterarStatusBotaoReiniciar();
+
+}
+
+//classList.toggle() é usado para adicionar uma classe se ela não existe e remover se ela já existe.
+//Se o botão tiver btn-desativado, ele remove essa classe e adiciona btn-ativo e vice versa.
+Se ele já tiver btn-ativo, faz o oposto.
 function alterarStatusBotaoReiniciar() {
     const botao = document.getElementById('btn-reiniciar');
     botao.classList.toggle('container__botao');
     botao.classList.toggle('container__botao-desabilitado');
 }
-    
-
-
-  
-    function reiniciar() {
-        quantidadeInput.value = '';
-        deInput.value = '';
-        ateInput.value = '';
-        numerosSorteados.innerHTML = '<label class="texto__paragrafo">Sorteio reiniciado, inicie um novo </label>';
-        alterarStatusBotaoReiniciar();
-        console.log('Campos reiniciados.');
-    }
+     
+function reiniciar() {
+    quantidadeInput.value = '';
+    deInput.value = '';
+    ateInput.value = '';
+    numerosSorteados.innerHTML = '<label class="texto__paragrafo">Sorteio reiniciado, inicie um novo </label>';
+    alterarStatusBotaoReiniciar();
+    console.log('Campos reiniciados.');
+}
     
 
 
